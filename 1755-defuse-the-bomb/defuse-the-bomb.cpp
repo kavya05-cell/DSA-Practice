4e1f6a0a639eb2a5238c9c7be1050fd3ffc20ed7
CoderK <rajputkavya410@gmail.com>
2026-03-24 20:35:01 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (50.95%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -2,19 +2,28 @@ class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
         int n=code.size();
+        int s,e;
+        int sum=0;
         vector<int>arr(n,0);
         if(k==0) return arr;;
+        if(k>0){
+            s=1;
+            e=k;
+        }
+        else{
+            s=n+k;
+            e=n-1;
+        }
+        for(int i=s;i<=e;i++){
+            sum+=code[i%n];
+        }
         for(int i=0;i<n;i++){
-            int sum=0;
-            if(k>0){
-                for(int j=1;j<=k;j++)
-                    sum+=code[(i+j)%n];
-            }
-            else{
-                for(int j=1;j<=abs(k);j++)
-                    sum+=code[(i-j+n)%n];
-            }
             arr[i]=sum;
+            sum-=code[s%n];
+            sum+=code[(e+1)%n];
+            s++;
+            e++;
+            
         }
         return arr;
     }
