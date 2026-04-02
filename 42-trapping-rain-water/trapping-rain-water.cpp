cdd2abe17c21717e9243d0e6bf370071ea45a4c9
CoderK <rajputkavya410@gmail.com>
2026-04-02 22:49:46 +0530

    Time: 41 ms (5.27%) | Memory: 38.3 MB (6.55%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -6,13 +6,13 @@ public:
         unordered_map<int,int>lm;
         unordered_map<int,int>rm;
         lm[0]=height[0];
-        rm[0]=height[n-1];
+        rm[n-1]=height[n-1];
         for(int i=1;i<n;i++)
             lm[i]=max(lm[i-1],height[i]);
-        for(int i=n-1;i>0;i--)
+        for(int i=n-2;i>=0;i--)
             rm[i]=max(rm[i+1],height[i]);
         for(int i=0;i<n;i++)
-            water+=max(0,min(lm[i],rm[i])-height[i]);
+            water+=(min(lm[i],rm[i])-height[i]);
         return water;
     }
 };
