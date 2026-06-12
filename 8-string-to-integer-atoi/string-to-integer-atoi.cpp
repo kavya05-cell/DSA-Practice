6443e306a9380197db7effced903a24597f95434
CoderK <rajputkavya410@gmail.com>
2026-06-12 22:51:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (54.28%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+const int minn= -2147483648;
+const int maxx= 2147483647;
+int rec(const string &s, int i, long long num, int sign){
+    if(i>=s.size() || !isdigit(s[i])) return (int)(sign*num);
+    num=num*10+(s[i]-'0');
+    if(sign*num<=minn) return minn;
+    if(sign*num>=maxx) return maxx;
+    return rec(s,i+1,num,sign);
+}
+    int myAtoi(string s) {
+        int i=0;
+        while(i<s.size() && s[i]==' ') i++;
+        int sign=1;
+        if(i<s.size() && (s[i]=='+' || s[i]=='-')){
+            sign=(s[i]=='-') ? -1:1;
+            i++;
+        }
+        return rec(s,i,0,sign);
+    }
+};
