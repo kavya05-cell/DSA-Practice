3bb809c9662b14995b72aab8e7157dcea5452bd1
CoderK <rajputkavya410@gmail.com>
2026-07-13 21:15:20 +0530

    Time: 3 ms (25.99%) | Memory: 11.2 MB (17.42%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        if(nums.empty()) return {{}};
-        vector<int>temp=vector<int>(nums.begin()+1,nums.end());
-        vector<vector<int>>perms=permute(temp);
-        vector<vector<int>>res;
-        for(const auto& p:perms){
-            for(int i=0;i<=p.size();i++){
-                vector<int>pcopy=p;
-                pcopy.insert(pcopy.begin()+i,nums[0]);
-                res.push_back(pcopy);
+        vector<vector<int>>perms={{}};
+        for(int num:nums){
+            vector<vector<int>>newperms;
+            for(const auto& p:perms){
+                for(int i=0;i<=p.size();i++){
+                    vector<int>pcopy=p;
+                    pcopy.insert(pcopy.begin()+i,num);
+                    newperms.push_back(pcopy);
+                }
             }
+            perms=newperms;
         }
-        return res;
+        return perms;
     }
 };
