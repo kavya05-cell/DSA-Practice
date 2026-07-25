e498a33f4553922e3aa4b08f702cf9c70a3c04ae
CoderK <rajputkavya410@gmail.com>
2026-07-25 23:27:14 +0530

    Time: 1 ms (81.87%) | Memory: 12 MB (62.13%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -1,28 +1,27 @@
 class Solution {
-    vector<bool> used;
-    int target;
+    int target, n;
 
 public:
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (sum % k != 0) return false;
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        if (total % k != 0) return false;
 
-        target = sum / k;
+        target = total / k;
+        n = nums.size();
         sort(nums.rbegin(), nums.rend());
-        used.assign(nums.size(), false);
-        return backtrack(nums, k, 0, 0);
+        return backtrack(nums, 0, k, 0, (1 << n) - 1);
     }
 
 private:
-    bool backtrack(vector<int>& nums, int k, int currentSum, int start) {
+    bool backtrack(vector<int>& nums, int i, int k, int subsetSum, int mask) {
         if (k == 0) return true;
-        if (currentSum == target) return backtrack(nums, k - 1, 0, 0);
-
-        for (int i = start; i < nums.size(); i++) {
-            if (used[i] || currentSum + nums[i] > target) continue;
-            used[i] = true;
-            if (backtrack(nums, k, currentSum + nums[i], i + 1)) return true;
-            used[i] = false;
+        if (subsetSum == target) return backtrack(nums, 0, k - 1, 0, mask);
+        for (int j = i; j < nums.size(); j++) {
+            if ((mask & (1 << j)) == 0 || subsetSum + nums[j] > target) continue;
+            if (backtrack(nums, j + 1, k, subsetSum + nums[j], mask ^ (1 << j))) {
+                return true;
+            }
+            if (subsetSum == 0) return false;
         }
         return false;
     }
