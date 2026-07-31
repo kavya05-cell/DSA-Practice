09cce3f9a76f4e2a266335c3c9f2623b8b4c194d
CoderK <rajputkavya410@gmail.com>
2026-07-31 15:36:03 +0530

    Time: 37 ms (86.94%) | Memory: 112.6 MB (24.81%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,23 +1,57 @@
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isWord;
+
+    TrieNode() {
+        for (int i = 0; i < 26; ++i) children[i] = nullptr;
+        isWord = false;
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void addWord(const string& word) {
+        TrieNode* curr = root;
+        for (char c : word) {
+            if (!curr->children[c - 'a']) {
+                curr->children[c - 'a'] = new TrieNode();
+            }
+            curr = curr->children[c - 'a'];
+        }
+        curr->isWord = true;
+    }
+};
+
 class Solution {
 public:
     int minExtraChar(string s, vector<string>& dictionary) {
-        unordered_set<string> words(dictionary.begin(), dictionary.end());
-        int n = s.size();
-        vector<int> dp(n + 1, -1);
-        dp[n] = 0;
-        return dfs(0, s, words, dp);
-    }
+        Trie trie;
+        for (const string& word : dictionary) {
+            trie.addWord(word);
+        }
 
-private:
-    int dfs(int i, string& s, unordered_set<string>& words, vector<int>& dp) {
-        if (dp[i] != -1) return dp[i];
-        int res = 1 + dfs(i + 1, s, words, dp);
-        for (int j = i; j < s.size(); j++) {
-            if (words.count(s.substr(i, j - i + 1))) {
-                res = min(res, dfs(j + 1, s, words, dp));
+        int n = s.size();
+        vector<int> dp(n + 1);
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i] = 1 + dp[i + 1];
+            TrieNode* curr = trie.root;
+
+            for (int j = i; j < n; ++j) {
+                if (!curr->children[s[j] - 'a']) break;
+                curr = curr->children[s[j] - 'a'];
+                if (curr->isWord) {
+                    dp[i] = min(dp[i], dp[j + 1]);
+                }
             }
         }
-        dp[i] = res;
-        return res;
+
+        return dp[0];
     }
 };
