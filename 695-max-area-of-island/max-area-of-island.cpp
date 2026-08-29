a1fc3768e58c6406d244be1a42d015db48e9d31a
CoderK <rajputkavya410@gmail.com>
2026-08-29 15:09:33 +0530

    Time: 4 ms (38.15%) | Memory: 28.6 MB (38.45%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,33 +1,71 @@
+class DSU {
+    vector<int> Parent, Size;
+public:
+    DSU(int n) {
+        Parent.resize(n + 1);
+        Size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            Parent[i] = i;
+            Size[i] = 1;
+        }
+    }
+
+    int find(int node) {
+        if (node != Parent[node]) {
+            Parent[node] = find(Parent[node]);
+        }
+        return Parent[node];
+    }
+
+    bool unionBySize(int u, int v) {
+        int pu = find(u);
+        int pv = find(v);
+        if (pu == pv) return false;
+        if (Size[pu] >= Size[pv]) {
+            Size[pu] += Size[pv];
+            Parent[pv] = pu;
+        } else {
+            Size[pv] += Size[pu];
+            Parent[pu] = pv;
+        }
+        return true;
+    }
+
+    int getSize(int node) {
+        return Size[find(node)];
+    }
+};
+
 class Solution {
-    int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int ROWS = grid.size(), COLS = grid[0].size();
+        int ROWS = grid.size();
+        int COLS = grid[0].size();
+        DSU dsu(ROWS * COLS);
+
+        int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         int area = 0;
+
+        auto index = [&](int r, int c) {
+            return r * COLS + c;
+        };
 
         for (int r = 0; r < ROWS; r++) {
             for (int c = 0; c < COLS; c++) {
                 if (grid[r][c] == 1) {
-                    area = max(area, dfs(grid, r, c));
+                    for (auto& d : directions) {
+                        int nr = r + d[0];
+                        int nc = c + d[1];
+                        if (nr >= 0 && nc >= 0 && nr < ROWS &&
+                            nc < COLS && grid[nr][nc] == 1) {
+                            dsu.unionBySize(index(r, c), index(nr, nc));
+                        }
+                    }
+                    area = max(area, dsu.getSize(index(r, c)));
                 }
             }
         }
 
         return area;
-    }
-
-    int dfs(vector<vector<int>>& grid, int r, int c) {
-        if (r < 0 || c < 0 || r >= grid.size() ||
-            c >= grid[0].size() || grid[r][c] == 0) {
-            return 0;
-        }
-
-        grid[r][c] = 0;
-        int res = 1;
-        for (int i = 0; i < 4; i++) {
-            res += dfs(grid, r + directions[i][0],
-                             c + directions[i][1]);
-        }
-        return res;
     }
 };
