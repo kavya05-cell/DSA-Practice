5eef39a2c6ded910d600fb852145c3a74fb2bdde
CoderK <rajputkavya410@gmail.com>
2026-09-03 11:12:19 +0530

    Time: 39 ms (39.22%) | Memory: 32 MB (6.74%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,35 +1,33 @@
 class Solution {
-    vector<vector<int>> adj;
-    unordered_map<int, unordered_set<int>> prereqMap;
-
 public:
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        adj.assign(numCourses, vector<int>());
+        vector<unordered_set<int>> adj(numCourses), isPrereq(numCourses);
+        vector<int> indegree(numCourses, 0);
+
         for (auto& pre : prerequisites) {
-            adj[pre[1]].push_back(pre[0]);
+            adj[pre[0]].insert(pre[1]);
+            indegree[pre[1]]++;
         }
-        for (int crs = 0; crs < numCourses; crs++) {
-            dfs(crs);
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) q.push(i);
+        }
+
+        while (!q.empty()) {
+            int node = q.front(); q.pop();
+            for (int neighbor : adj[node]) {
+                isPrereq[neighbor].insert(node);
+                isPrereq[neighbor].insert(isPrereq[node].begin(), isPrereq[node].end());
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) q.push(neighbor);
+            }
         }
 
         vector<bool> res;
         for (auto& query : queries) {
-            res.push_back(prereqMap[query[1]].count(query[0]));
+            res.push_back(isPrereq[query[1]].count(query[0]));
         }
         return res;
-    }
-
-private:
-    unordered_set<int>& dfs(int crs) {
-        if (prereqMap.count(crs)) {
-            return prereqMap[crs];
-        }
-        prereqMap[crs] = unordered_set<int>();
-        for (int pre : adj[crs]) {
-            auto& cur = dfs(pre);
-            prereqMap[crs].insert(cur.begin(), cur.end());
-        }
-        prereqMap[crs].insert(crs);
-        return prereqMap[crs];
     }
 };
