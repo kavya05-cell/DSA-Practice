2d922f2fac1dd134cb9519ca8a2bfff71409cae3
CoderK <rajputkavya410@gmail.com>
2026-09-12 01:09:21 +0530

    Time: 60 ms (30.41%) | Memory: 94 MB (38.75%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
-            return a[1] < b[1];
-        });
-        int n = intervals.size();
-        vector<int> dp(n);
-        dp[0] = 1;
+        sort(intervals.begin(), intervals.end());
+        int res = 0;
+        int prevEnd = intervals[0][1];
 
-        for (int i = 1; i < n; i++) {
-            int idx = bs(i, intervals[i][0], intervals);
-            if (idx == 0) {
-                dp[i] = dp[i - 1];
+        for (int i = 1; i < intervals.size(); i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            if (start >= prevEnd) {
+                prevEnd = end;
             } else {
-                dp[i] = max(dp[i - 1], 1 + dp[idx - 1]);
+                res++;
+                prevEnd = min(end, prevEnd);
             }
         }
-        return n - dp[n - 1];
-    }
-
-    int bs(int r, int target, vector<vector<int>>& intervals) {
-        int l = 0;
-        while (l < r) {
-            int m = (l + r) >> 1;
-            if (intervals[m][1] <= target) {
-                l = m + 1;
-            } else {
-                r = m;
-            }
-        }
-        return l;
+        return res;
     }
 };
