9505ee9b7615f1e1205972ac8d691572080b8d37
CoderK <rajputkavya410@gmail.com>
2026-09-14 20:50:23 +0530

    Time: 0 ms (100.00%) | Memory: 150.6 MB (75.03%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
-        unordered_set<int>good;
+        bool x=false, y=false, z=false;
         for(const auto& t:triplets){
-            if(t[0]>target[0] || t[1]>target[1] || t[2]>target[2]) continue;
-            for(int i=0;i<t.size();i++){
-                if(t[i]==target[i]) good.insert(i);
-            }
+            x|=(t[0]==target[0] && t[1]<=target[1] && t[2]<=target[2]);
+            y|=(t[0]<=target[0] && t[1]==target[1] && t[2]<=target[2]);
+            z|=(t[0]<=target[0] && t[1]<=target[1] && t[2]==target[2]);
+            if(x && y && z) return true;
         }
-        return good.size()==3;
+        return false;
     }
 };
