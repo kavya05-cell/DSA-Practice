1ebe29a039ab13b41059e08dbc547f4eee23bf8b
CoderK <rajputkavya410@gmail.com>
2026-09-15 10:28:25 +0530

    Time: 90 ms (50.50%) | Memory: 125.7 MB (93.54%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -2,37 +2,26 @@ class Solution {
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
         sort(meetings.begin(), meetings.end());
-        vector<long> rooms(n, 0); // end times of meetings in rooms
-        vector<int> meetingCount(n, 0);
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> available;
+        for (int i = 0; i < n; i++) {
+            available.push({0, i});
+        }
+        vector<int> count(n);
 
         for (const auto& meeting : meetings) {
             int start = meeting[0], end = meeting[1];
-            int minRoom = 0;
-            bool found = false;
-
-            for (int i = 0; i < n; i++) {
-                if (rooms[i] <= start) {
-                    meetingCount[i]++;
-                    rooms[i] = end;
-                    found = true;
-                    break;
-                }
-                if (rooms[minRoom] > rooms[i]) {
-                    minRoom = i;
-                }
+            while (!available.empty() && available.top().first < start) {
+                auto [end_time, room] = available.top();
+                available.pop();
+                available.push({start, room});
             }
 
-            if (found) continue;
-            meetingCount[minRoom]++;
-            rooms[minRoom] += end - start;
+            auto [end_time, room] = available.top();
+            available.pop();
+            available.push({end_time + (end - start), room});
+            count[room]++;
         }
 
-        int maxIndex = 0;
-        for (int i = 1; i < n; i++) {
-            if (meetingCount[i] > meetingCount[maxIndex]) {
-                maxIndex = i;
-            }
-        }
-        return maxIndex;
+        return max_element(count.begin(), count.end()) - count.begin();
     }
 };
