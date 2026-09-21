67f1b49616d5f5c5f2f00881a19d20055be15119
CoderK <rajputkavya410@gmail.com>
2026-09-21 19:50:41 +0530

    Time: 3 ms (99.61%) | Memory: 14.3 MB (79.19%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    vector<vector<int>> memo;
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         for (int num : nums) {
@@ -9,24 +8,15 @@ public:
         if (sum % 2 != 0) {
             return false;
         }
-        memo.resize(nums.size(), vector<int>(sum / 2 + 1, -1));
 
-        return dfs(nums, 0, sum / 2);
-    }
+        int target = sum / 2;
+        bitset<10001> dp;
+        dp[0] = 1;
 
-    bool dfs(vector<int>& nums, int i, int target) {
-        if (i == nums.size()) {
-            return target == 0;
-        }
-        if (target < 0) {
-            return false;
-        }
-        if (memo[i][target] != -1) {
-            return memo[i][target];
+        for (int num : nums) {
+            dp |= dp << num;
         }
 
-        memo[i][target] =  dfs(nums, i + 1, target) ||
-                           dfs(nums, i + 1, target - nums[i]);
-        return memo[i][target];
+        return dp[target];
     }
 };
